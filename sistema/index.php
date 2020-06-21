97dcb6b2d66681fd6260c29d630a42c38d7d27c8
mani <maniortiz98@gmail.com>
2020-06-21 18:56:15 -0500

    16-fact

---
+++ sistema/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include "scripts.php" ?>
+    <title>Sistema</title>
+  </head>
+  <body>
+
+  <?php include "header.php" ?>
+
+
+
+
+ 
+  <?php include "footer.php" ?>
+   </body>
+</html>
